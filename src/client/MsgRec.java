f252164cb074ce746b439a8df998d88a81fe1431
neocuni <neocuni@hotmail.com>
2018-11-14 21:00:05 +0200

    jaa &                System.out.println(ChatClient.communication.receiveMessage()) Please enter the commit message for your changes. Lines starting

--- src/client/MsgRec.java
+++ src/client/MsgRec.java
@@ -5,23 +5,24 @@ import java.io.InputStreamReader;
 import java.net.Socket;
 
 public class MsgRec extends Thread {
-    Socket s;
-    public MsgRec(Socket socket) {
-        this.s = socket;
+//  Socket s;
+    public MsgRec() {
+//      this.s = socket;
     }
 
     public void run() {
         try {
-            BufferedReader reader = new BufferedReader(new InputStreamReader( s.getInputStream() ) );
+//          BufferedReader reader = new BufferedReader(new InputStreamReader( s.getInputStream() ) );
             while(true) {
-                String msg = reader.readLine();
-                System.out.println(msg);
+                System.out.println(ChatClient.communication.receiveMessage());
+//              String msg = reader.readLine();
+//              System.out.println(msg);
 //              ChatClient.taMessages.appendText(msg+"\n");
 //              ChatClient.taMessages.setScrollTop(Double.MAX_VALUE);
             }
             
         } catch (Exception er) {
-
+        	System.out.println("MsgRec error");
         }
     }
 }
