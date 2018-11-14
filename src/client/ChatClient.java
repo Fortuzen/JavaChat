d70218efdd5f7b86c11a7e596d373d37809ff7be
neocuni <neocuni@hotmail.com>
2018-11-14 23:43:17 +0200

    ddd

--- src/client/ChatClient.java
+++ src/client/ChatClient.java
@@ -1,46 +1,128 @@
 package client;
 
 import java.net.Socket;
-import java.util.Scanner;
+import java.util.Optional;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextInputDialog;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Popup;
+import javafx.stage.Stage;
 
 import shared.ICommunication;
 import shared.DefaultCommunication;
 
 
-public class ChatClient {
+public class ChatClient extends Application {
+	
 	public static ICommunication communication = null;
+    public static TextArea taMessages;
+	Socket socket;
+	
+    public void start(Stage primaryStage) {
+		
+        MenuBar menu = new MenuBar();
+
+        Menu menu1 = new Menu("Connect...");
+        Menu menu2 = new Menu("Help");
+        menu.getMenus().add(menu1);
+        menu.getMenus().add(menu2);
+
+        MenuItem menuItem = new MenuItem("Quick connect");
+        MenuItem menuItem2 = new MenuItem("Connect to a server");
+        menu1.getItems().add(menuItem);
+        menu1.getItems().add(menuItem2);
+        
+        MenuItem menu2Item = new MenuItem("Help");
+        menu2.getItems().add(menu2Item);
+
+        taMessages = new TextArea();
+        taMessages.setEditable(false);
+        taMessages.setWrapText(true);
+
+        TextArea taInput = new TextArea();
+
+        BorderPane border = new BorderPane();
+        border.setTop(menu);
+        border.setCenter(taMessages);
+        border.setBottom(taInput);
+        
+        Scene mainFrame = new Scene(border, 800,600);
+
+        primaryStage.setTitle("Chat Client");
+        primaryStage.setScene(mainFrame);
+        primaryStage.centerOnScreen();
+        primaryStage.show();
+        
+        menuItem.setOnAction(e->{
+    		String ip = "localhost";
+    		int port = 8000;
+            try {
+                socket = new Socket(ip, port);
+                System.out.println("Connected to " + ip + " " + port);
+                communication = new DefaultCommunication(socket);
+                MsgRec rec = new MsgRec();
+                rec.start(); // Listen to server
+
+            } catch (Exception error) {
+                System.out.println("Could not connect to " + ip + " " + port);
+            }
+
+        });
+        
+
+        
+        menuItem2.setOnAction(e->{
+        	TextInputDialog dialog = new TextInputDialog("localhost");
+        	dialog.setTitle("Connect to a server");
+        	dialog.setHeaderText("Enter server IP address");
+        	dialog.setContentText("IP address:");
+
+        	Optional<String> result = dialog.showAndWait();
+        	if (result.isPresent()){
+        		String ip = result.get();
+        		int port = 8000; // TODO: Change to multiple field dialog
+                try {
+                    socket = new Socket(ip, port);
+                    System.out.println("Connected to " + ip + " " + port);
+                    communication = new DefaultCommunication(socket);
+                    MsgRec rec = new MsgRec();
+                    rec.start(); // Listen to server
+
+                } catch (Exception error) {
+                    System.out.println("Could not connect to " + ip + " " + port);
+                }
+        	}
+        });
+        
+        menu2Item.setOnAction(e-> {
+        	//TODO: display helpful stuff
+        });
+
+        taInput.setOnKeyPressed(e -> {
+            if(!e.getCode().equals(KeyCode.ENTER)) return;
+            if(taInput.getText().isEmpty()) return;
+            String msg = taInput.getText();   
+            try {
+                communication.sendMessage(msg); // Send message to server
+            } catch (Exception er) {
+            	System.out.println("ERROR: Could not send a message");
+            } 
+            taInput.setText("");
+        });
+	}
 	
 	public static void main(String[] args) {
-
-        Scanner scanner = new Scanner(System.in);
-		String ip = "localhost";
-		int port = 8000;
-		Socket socket;
-//		System.out.print("Port: ");
-//		port = scanner.nextInt();
-        try {
-            socket = new Socket(ip, port);
-            System.out.println("Connected to " + ip + " " + port);
-            communication = new DefaultCommunication(socket);
-            MsgRec rec = new MsgRec();
-            rec.start();
-
-        } catch (Exception error) {
-            System.out.println("Could not connect to " + ip + " " + port);
-        }
-
-        
-        System.out.print("Insert nickname: ");
-        String nick = scanner.next();
-        communication.sendMessage(nick);
-        
-        String msg = "";
-        while (!(msg.contains("quit"))) {
-        	msg = scanner.nextLine();
-            communication.sendMessage(msg);
-        }
-        
-        scanner.close();
-
+		Application.launch(args);
     }
 }
--- src/client/MsgRec.java
+++ src/client/MsgRec.java
@@ -1,24 +1,17 @@
 package client;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.Socket;
+import java.sql.Timestamp;
 
 public class MsgRec extends Thread {
-//  Socket s;
     public MsgRec() {
-//      this.s = socket;
     }
 
     public void run() {
         try {
-//          BufferedReader reader = new BufferedReader(new InputStreamReader( s.getInputStream() ) );
             while(true) {
-                System.out.println(ChatClient.communication.receiveMessage());
-//              String msg = reader.readLine();
-//              System.out.println(msg);
-//              ChatClient.taMessages.appendText(msg+"\n");
-//              ChatClient.taMessages.setScrollTop(Double.MAX_VALUE);
+            	String msg = ChatClient.communication.receiveMessage();
+                ChatClient.taMessages.appendText("[" + new Timestamp(System.currentTimeMillis()).toLocalDateTime() + "] " + msg+"\n"); //TODO: Timestamp too precise
+                ChatClient.taMessages.setScrollTop(Double.MAX_VALUE);
             }
             
         } catch (Exception er) {
