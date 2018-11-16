ccc5deb9acbd2ec7e8088d2d7f0e5ed4458ab45a
Antti Neuvonen <anttine@student.uef.fi>
2018-11-16 14:32:50 +0200

    Remove CQuit dependency on CLeaveroom

--- src/server/commands/CQuit.java
+++ src/server/commands/CQuit.java
@@ -6,14 +6,21 @@ import server.Room;
 import server.User;
 
 import java.util.ArrayList;
-// Depends on CLeaveroom
+
 public class CQuit implements server.ICommand {
     @Override
     public void execute(ChatServerThread chatServerThread, String msg) {
-        User user = chatServerThread.user;
-        ChatServer.commands.get("/leaveroom").execute(chatServerThread, msg);
+        // Partially from CLeaveroom.java. 
+        Room room = chatServerThread.user.currentRoom;
+        if(room!=null) {
+            chatServerThread.sendMessageToCurrentRoom((chatServerThread.user.name + " left the room " + room.roomSettings.name), "SERVER");
+            room.users.remove(chatServerThread.user);
+            chatServerThread.user.currentRoom = null;
+            chatServerThread.user.mode = (chatServerThread.user.mode >= 3) ? chatServerThread.user.mode : 0;
+        }
+
         try {
-            user.socket.close();
+            chatServerThread.user.socket.close();
         } catch (Exception e) {
             //TODO: handle exception
         }      
