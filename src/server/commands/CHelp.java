066b9eb094b84a501ec02f31ce1822d7854a06bd
Antti Neuvonen <anttine@student.uef.fi>
2018-11-18 01:10:22 +0200

    Fix Chelp

--- src/server/commands/CHelp.java
+++ src/server/commands/CHelp.java
@@ -1,20 +1,22 @@
 package server.commands;
 
 import server.ChatServer;
+import server.ICommand;
 import server.ChatServer.ChatServerThread;
 import server.Room;
 import java.util.ArrayList;
+import java.util.Map;
 
 public class CHelp implements server.ICommand {
     @Override
     public void execute(ChatServerThread chatServerThread, String msg) {
-        try {
-            chatServerThread.user.getCommunication().sendMessage("**********\nHere are the available commands:\n/help - View commands"
-            + "\n/joinroom roomname - Join a room\n/leaveroom - Leave the current room\n/privmsg name msg - Send a private message to a user"
-            + "\n/quit - Close the chat\n/users - Show users currently in room\n/motd - Show message of the day\n**********"); //TODO: add all cmds, replace with more modular solution
-        } catch (Exception e) {
-            //TODO: handle exception
+        String helptext = "All commands:\n";
+
+        for(Map.Entry<String,ICommand> cmd : ChatServer.commands.entrySet()) {
+            helptext += cmd.getValue().getInfo()+"\n";
         }
+        chatServerThread.sendMessageToUser(helptext);
+
     }
     @Override
 	public String getInfo() {
